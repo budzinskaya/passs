ed900524d9097b0a69b51024a44c7a28012a56cb
budzinskaya <42929316+budzinskaya@users.noreply.github.com>
2023-05-31 20:36:11 +0300

    Delete RPG directory

--- RPG/Warrior_class.py
+++
@@ -1,6 +0,0 @@
-from Player_class import Player 
-
-class Warrior(Player):
-	def __init__(self, name):
-		super().__init__(name)
-		self.__player_class = "Warrior"
--- RPG/Wizard_class.py
+++
@@ -1,6 +0,0 @@
-from Player_class import Player 
-
-class Wizard(Player):
-	def __init__(self, name):
-		super().__init__(name)
-		self.__player_class = "Wizard"
